562a7a094b7d56a7dd760dc2e093cc59b6a1b7c6
Dieniffer Vargas <87767872+dieniffervargas@users.noreply.github.com>
2023-02-16 15:14:56 -0300

    feat: new exercises

---
+++ bee1005.py
@@ -0,0 +1,6 @@
+val1 = float(input())
+val2 = float(input())
+
+media = ((val1 * 3.5)+(val2 * 7.5)) / (3.5+7.5)
+
+print("MEDIA = %0.5f" % media)
---
+++ bee1006.py
@@ -0,0 +1,9 @@
+val1 = float(input())
+val2 = float(input())
+val3 = float(input())
+
+media = (val1*0.2) + (val2*0.3) + (val3*0.5)
+
+texto = "MEDIA = %0.1f" % (media)
+
+print(texto)
