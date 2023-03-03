80d3835bf9c94f9879773187f6e5b7ddbec06ac1
Dieniffer Vargas <87767872+dieniffervargas@users.noreply.github.com>
2023-03-03 16:15:33 -0300

    fix: exercise 1010

--- b331010.py
+++ b331010.py
@@ -4,8 +4,17 @@ entrada_b = input().split(" ")
 cod_a,qnt_a,val_a = entrada_a
 cod_b,qnt_b,val_b = entrada_b
 
-print(cod_a)
 total = (int(qnt_a) * float(val_a)) + (int(qnt_b) * float(val_b))
 texto = "VALOR A PAGAR: R$ %0.2f" % (total)
 
 print(texto)
+
+
+//sdkajsdkjashdakjhdkajs
+
+x = input().split(" ")
+y= input().split(" ")
+a,b,c = x
+d,e,f= y
+total = (int(b) * float(c)) + (int(e) * float(f))
+print("VALOR A PAGAR: R$ %0.2f" %total)
