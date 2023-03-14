6ce8dff728c9c8711d1602c1fc12d3c54bd0f37d
Dieniffer Vargas <87767872+dieniffervargas@users.noreply.github.com>
2023-03-14 20:26:23 -0300

    fix: exercise 1036 working

--- bee1036.py
+++ bee1036.py
@@ -1,23 +1,22 @@
-import cmath
-val = input()
+# val = input()
+# a,b,c = val.split(" ")
+# a = float(a)
+# b = float(b)
+# c = float(c)
 
-a,b,c = val.split(" ")
-a = float(a)
-b = float(b)
-c = float(c)
+a, b, c = map(float, input().split())
 
 delta = ((b**2)-4*a*c)
 
 if a == 0 or delta < 0:
     print("Impossivel calcular")
-
 elif (delta == 0):
     x1 = (-b + delta**(1/2)) / (2 * a)
     x2 = x1
-    print("R1 = %0.5f" % x1)
-    print("R2 = %0.5f" % x2)
+    print("R1 = %0.5f" % (x1))
+    print("R2 = %0.5f" % (x2))
 else:
    x1 = (-b + delta**(1/2)) / (2 * a)
    x2 = (-b - delta**(1/2)) / (2 * a)
-   print("R1 = %0.5f" % x1)
-   print("R2 = %0.5f" % x2)
+   print("R1 = %0.5f" % (x1))
+   print("R2 = %0.5f" % (x2))
